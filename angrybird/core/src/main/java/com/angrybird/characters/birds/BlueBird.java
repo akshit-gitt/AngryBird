dfe59cc5ffdd0be9383dee92c7e013d673cc977a
akshit-gitt <162747319+akshit-gitt@users.noreply.github.com>
2024-11-22 15:48:36 +0530

    Update BlueBird.java

--- angrybird/core/src/main/java/com/angrybird/characters/birds/BlueBird.java
+++ angrybird/core/src/main/java/com/angrybird/characters/birds/BlueBird.java
@@ -14,6 +14,7 @@ public class BlueBird extends Bird{
         bodyDef.type = BodyDef.BodyType.DynamicBody;
         bodyDef.position.set(xpos, ypos);
         body = world.createBody(bodyDef);
+        body.setUserData(this);
         CircleShape shape = new CircleShape();
         shape.setRadius(3.5f); // Adjust radius to bird size
         FixtureDef fixtureDef = new FixtureDef();
